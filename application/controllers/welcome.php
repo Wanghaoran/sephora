43a8a093977af9ac2eeba9979b7961b0f0935a00
WangHaoRan <whrlmc@139.com>
2015-03-16 09:28:36 +0800

    1

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -19,7 +19,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+        echo $_SERVER['HTTP_USER_AGENT'];
+//		$this->load->view('welcome_message');
 	}
 }
 
