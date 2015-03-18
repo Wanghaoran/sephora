70e8c8a7be419f62f3d1477a128764e32115ef70
WangHaoRan <whrlmc@139.com>
2015-03-18 21:28:08 +0800

    2323

--- application/controllers/user.php
+++ application/controllers/user.php
@@ -29,36 +29,9 @@ class User extends CI_Controller {
     public function answer(){
 
         $q = $this->input->get('q');
-        $question_arr = array(
-            '1' => array(
-                '1' => '答案1-1',
-                '2' => '答案1-2',
-                '3' => '答案1-3',
-            ),
-            '2' => array(
-                '1' => '答案2-1',
-                '2' => '答案2-2',
-                '3' => '答案2-3',
-            ),
-            '3' => array(
-                '1' => '答案3-1',
-                '2' => '答案3-2',
-                '3' => '答案3-3',
-            ),
-            '4' => array(
-                '1' => '答案4-1',
-                '2' => '答案4-2',
-                '3' => '答案4-3',
-            ),
-            '5' => array(
-                '1' => '答案5-1',
-                '2' => '答案5-2',
-                '3' => '答案5-3',
-            ),
-        );
 
         $data = array(
-            'answer_arr' => $question_arr[$q],
+            'answer_arr' => $this -> config -> item('questions')[$q]['answer'],
             'q' => $q,
         );
         $this->load->view('answer',$data);
