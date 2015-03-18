8bd0b942e176f04c114c21840b74cbdcf5b261e0
WangHaoRan <whrlmc@139.com>
2015-03-18 21:42:00 +0800

    123123

--- application/views/customquestion.php
+++ application/views/customquestion.php
@@ -68,6 +68,18 @@
 </script>
 <script>
     var checkup = function(){
+        if($("input[name='ques']").val()){
+            alert('问题不能为空！');
+        }
+        if($("input[name='ans1']").val()){
+            alert('答案一不能为空！');
+        }
+        if($("input[name='ans2']").val()){
+            alert('答案二不能为空！');
+        }
+        if($("input[name='ans3']").val()){
+            alert('答案三不能为空！');
+        }
         $('#form').submit();
     }
 </script>
