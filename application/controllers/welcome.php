1d28d494233a59b1c6d4d7493679b38210c1b3f5
WangHaoRan <whrlmc@139.com>
2015-03-18 15:49:11 +0800

    123123

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -58,9 +58,26 @@ class Welcome extends CI_Controller {
         if(empty($_GET['code'])){
             $token_url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx949efd128cd9bf73&redirect_uri=' . urlencode('http://sephora.cnhtk.cn/index.php/welcome/oauth2_authorize') . '&response_type=code&scope=snsapi_userinfo&state=question#wechat_redirect';
             redirect($token_url);
-        }else{
-            var_dump($_GET);
         }
+
+        //get token
+        $token_url = 'https://api.weixin.qq.com/sns/oauth2/access_token?appid=wx949efd128cd9bf73&secret=f5b75c8db05e107944f144b6eff2b304&code=' . $_GET['code'] . '&grant_type=authorization_code';
+        $result_json = file_get_contents($token_url);
+        $result_arr = json_decode($result_json, true);
+        if(!empty($result_arr['errcode'])){
+            die('<h1>Authorization failure1!' .  $result_arr['errmsg'] . '</h1>');
+        }
+
+        //get user info
+        $info_url = 'https://api.weixin.qq.com/sns/userinfo?access_token=' . $result_arr['access_token'] . '&openid=' . $result_arr['openid'] . '&lang=zh_CN';
+
+        $result_json = file_get_contents($info_url);
+        $result_arr = json_decode($result_json, true);
+        if(!empty($result_arr['errcode'])){
+            die('<h1>Authorization failure2!' .  $result_arr['errmsg'] . '</h1>');
+        }
+
+        var_dump($result_arr);
     }
 }
 
