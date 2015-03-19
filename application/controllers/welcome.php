47d77bc8d2e6c221f659eb4131bd0f70aaf559e9
WangHaoRan <whrlmc@139.com>
2015-03-19 15:19:09 +0800

    123123

--- application/controllers/welcome.php
+++ application/controllers/welcome.php
@@ -224,13 +224,15 @@ class Welcome extends CI_Controller {
         $this -> load -> model('questionuser_model');
 
         $icon_arr = $this -> questionuser_model -> selectusericon($q);
+
+
         if(!$icon_arr){
             $icon = $icon_arr[array_rand($icon_arr)]['headimgurl'];
         }else{
             $icno = '';
         }
 
-        var_dump($icon);
+        var_dump($icon_arr);
 
         $data = array(
             'code_arr' => array_merge($code10, $code30, $code50),
